ae0cf3fc3cb2b5a1509996150b23da048ccaf3e1
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2026-05-18 09:57:24 +0530

    feat: add language constants for supported programming languages in battle room

---
+++ client/src/pages/BattleRoomPage/battleConstants.js
@@ -0,0 +1,15 @@
+export const LANGUAGES = [
+  'javascript',
+  'python',
+  'java',
+  'cpp',
+  'go',
+];
+
+export const LANGUAGE_LABELS = {
+  javascript: 'JS',
+  python: 'PY',
+  java: 'JV',
+  cpp: 'C++',
+  go: 'GO',
+};
