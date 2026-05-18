091ccf090346c4309c75e84aebc89c268f3082d5
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2026-05-18 19:21:34 +0530

    feat: add autoprefixer as a dependency and update postcss configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   server: {
     host: '0.0.0.0',
+
     port: 5173,
+
     proxy: {
       '/api': {
-        target: 'http://backend:5000',
+        target: 'http://localhost:5000',
         changeOrigin: true,
       },
+
       '/socket.io': {
-        target: 'http://backend:5000',
+        target: 'http://localhost:5000',
         ws: true,
         changeOrigin: true,
       },
