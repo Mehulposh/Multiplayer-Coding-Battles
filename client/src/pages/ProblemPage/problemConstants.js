e93dca0cc21ec16a827cf7d123c238e9f6b5fd2c
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2026-05-20 11:24:52 +0530

    feat: add problem constants for difficulties, styles, and popular tags

---
+++ client/src/pages/ProblemPage/problemConstants.js
@@ -0,0 +1,41 @@
+export const DIFFICULTIES = [
+  'easy',
+  'medium',
+  'hard',
+];
+
+export const DIFFICULTY_STYLES = {
+  easy: {
+    text: 'text-green-400',
+    bg: 'bg-green-400/10',
+    border: 'border-green-400/30',
+  },
+
+  medium: {
+    text: 'text-yellow-400',
+    bg: 'bg-yellow-400/10',
+    border: 'border-yellow-400/30',
+  },
+
+  hard: {
+    text: 'text-red-400',
+    bg: 'bg-red-400/10',
+    border: 'border-red-400/30',
+  },
+};
+
+export const POPULAR_TAGS = [
+  'array',
+  'string',
+  'hash-table',
+  'dynamic-programming',
+  'math',
+  'sorting',
+  'binary-search',
+  'stack',
+  'recursion',
+  'two-pointers',
+  'linked-list',
+  'tree',
+  'graph',
+];
