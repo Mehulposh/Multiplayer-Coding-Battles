e544dd835524b7598560e138dcedbd5b41a134aa
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2026-05-20 15:16:31 +0530

    feat: add ELO_TIERS constant for player ranking tiers

---
+++ client/src/pages/ProfilePage/profileConstants.js
@@ -0,0 +1,49 @@
+export const ELO_TIERS = [
+  {
+    min: 2000,
+    label: 'Grandmaster',
+    color: 'text-yellow-400',
+    bg: 'bg-yellow-400/10',
+    border: 'border-yellow-400/30',
+  },
+
+  {
+    min: 1600,
+    label: 'Master',
+    color: 'text-purple-400',
+    bg: 'bg-purple-400/10',
+    border: 'border-purple-400/30',
+  },
+
+  {
+    min: 1400,
+    label: 'Diamond',
+    color: 'text-blue-400',
+    bg: 'bg-blue-400/10',
+    border: 'border-blue-400/30',
+  },
+
+  {
+    min: 1200,
+    label: 'Platinum',
+    color: 'text-cyan-400',
+    bg: 'bg-cyan-400/10',
+    border: 'border-cyan-400/30',
+  },
+
+  {
+    min: 1000,
+    label: 'Gold',
+    color: 'text-yellow-500',
+    bg: 'bg-yellow-500/10',
+    border: 'border-yellow-500/30',
+  },
+
+  {
+    min: 0,
+    label: 'Silver',
+    color: 'text-slate-400',
+    bg: 'bg-slate-400/10',
+    border: 'border-slate-400/30',
+  },
+];
