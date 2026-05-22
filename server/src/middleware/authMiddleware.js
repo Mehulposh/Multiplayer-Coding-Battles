8d1dadcaa2d4bf20cd801ef1a28804d38bd5e113
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2026-05-22 10:27:23 +0530

    feat: add isAdmin middleware for admin role verification

--- server/src/middleware/authMiddleware.js
+++ server/src/middleware/authMiddleware.js
@@ -26,4 +26,11 @@ const protect = async (req, res, next) => {
   }
 };
 
-export { protect };
+//admin role checker
+const isAdmin = (req, res, next) => {
+  if (req.user?.role !== 'admin') {
+    return res.status(403).json({ message: 'Access denied. Admins only.' });
+  }
+  next();
+};
+export { protect , isAdmin};
